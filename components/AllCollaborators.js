8a2cebf2e56d52b0d667eebbab9465b30e40fcc2
Anya Dunaif <anya.dunaif@gmail.com>
2021-01-23 20:34:09 -0500

    added modal

--- components/AllCollaborators.js
+++ components/AllCollaborators.js
@@ -1,32 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import {
+  Avatar,
   Button,
-  Image,
-  ScrollView,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
-import { Colors, List } from 'react-native-paper';
+  Colors,
+  List,
+  Modal,
+  Portal,
+  Provider,
+} from 'react-native-paper';
 import collaborators from '../db/collaborators';
 
 const AllCollaborators = ({ navigation }) => {
+  const [visible, setVisible] = React.useState(false);
+
+  const showModal = () => setVisible(true);
+  const hideModal = () => setVisible(false);
+  const containerStyle = { backgroundColor: 'white', padding: 20 };
   return (
     <View style={styles.collabContainer}>
       <ScrollView>
         <List.Section>
           {collaborators.map((collaborator) => {
             return (
-              <List.Item
-                key={collaborator.id}
-                title={`${collaborator.first} ${collaborator.last}`}
-                description={`${collaborator.position}`}
-                onPress={() => navigation.navigate('collaborator')}
-                left={(props) => (
-                  <List.Icon {...props} color={Colors.cyan400} icon="account" />
-                )}
-              />
+              <>
+                <List.Item
+                  key={collaborator.id}
+                  title={`${collaborator.first} ${collaborator.last}`}
+                  description={`${collaborator.position}`}
+                  onPress={() => navigation.navigate('collaborator')}
+                  left={(props) => (
+                    <List.Icon
+                      {...props}
+                      color={Colors.cyan400}
+                      icon="account"
+                    />
+                  )}
+                />
+                <Provider>
+                  <Portal>
+                    <Modal
+                      visible={visible}
+                      onDismiss={hideModal}
+                      contentContainerStyle={containerStyle}
+                    >
+                      <Text
+                        style={styles.title}
+                      >{`${collaborator.first} ${collaborator.last}`}</Text>
+                    </Modal>
+                  </Portal>
+                  <Button onPress={showModal}>view</Button>
+                </Provider>
+              </>
             );
           })}
         </List.Section>
