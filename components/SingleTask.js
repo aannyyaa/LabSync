bb46f3819f02f092d215a706ec8a4b383c700039
Anya Dunaif <anya.dunaif@gmail.com>
2021-01-25 17:41:56 -0500

    added chips to single task

--- components/SingleTask.js
+++ components/SingleTask.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { RadioButton } from 'react-native-paper';
+import { Chip, RadioButton } from 'react-native-paper';
 
 const SingleTask = ({ route }) => {
   const {
@@ -28,12 +28,10 @@ const SingleTask = ({ route }) => {
       <Text style={styles.title}>{title}</Text>
       <Text style={styles.instructions}>{`Start date: ${startDate}`}</Text>
       <Text style={styles.instructions}>{`Deadline: ${deadline}`}</Text>
-      <Text
-        style={styles.instructions}
-      >{`Created by: ${creator.first} ${creator.last}`}</Text>
-      <Text
-        style={styles.instructions}
-      >{`Collaborators: me, ${allCollaboratorsStr}`}</Text>
+      <Text style={styles.instructions}>{`Collaborators:`}</Text>
+      {allCollaborators.map(collaborator => {
+        return <Chip style={styles.chip} key={collaborator} type="outlined" icon="account" onPress={() => console.log('Pressed')}>{collaborator}</Chip>
+      })}
       <RadioButton.Item
         label="Mark as complete"
         status={checked}
@@ -44,9 +42,6 @@ const SingleTask = ({ route }) => {
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
   title: {
     fontSize: 18,
     margin: '2%',
@@ -55,6 +50,9 @@ const styles = StyleSheet.create({
     fontSize: 15,
     margin: '2%',
   },
+  chip: {
+    width: "37%"
+  }
 });
 
 export default SingleTask;
